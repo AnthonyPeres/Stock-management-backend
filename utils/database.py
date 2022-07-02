cf112619bd3bf663057a8c3a0a48f9f3b48ef223
AnthonyPeres <anthony.peresdacruz@gmail.com>
2022-07-02 18:59:26 +0200

    Add and delete features

--- utils/database.py
+++ utils/database.py
@@ -9,14 +9,34 @@ def getTickers():
 def getTicker(ticker):
     print("Get : " + ticker)
 
+
 def addTicker(ticker):
     print("POST : " + ticker)
-    # with open('tickers.json', "r+") as json_file:
-    #     data = json.load(json_file)
-    #     data['tickers'].append(ticker)
-    #     json.dump(data, json_file)
+    with open('tickers.json', "r+") as json_file:
+        data = json.load(json_file)
+
+        if not ticker in data['tickers']:
+            data['tickers'].append(ticker)
+
+            with open('tickers.json', "w+") as json_file:
+                json.dump(data, json_file)
+        else:
+            print("Valeur déjà connue")
 
 
 def removeTicker(ticker):
     print("DEL : " + ticker)
 
+    with open('tickers.json', "r+") as json_file:
+        data = json.load(json_file)
+
+    try:
+        index = data['tickers'].index(ticker)
+
+        del data["tickers"][index]
+    
+        with open('tickers.json', "w+") as json_file:
+            json.dump(data, json_file)
+
+    except:
+        print('Valeur non connue')
